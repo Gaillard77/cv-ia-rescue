7fe0e5137a41d5ee943116b0c87cdc7b3f1963c0
Gaillard77 <spoorix@outlook.fr>
2025-10-09 19:37:10 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-export default function MyApp({ Component, pageProps }: AppProps){
+// pages/_app.jsx
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
