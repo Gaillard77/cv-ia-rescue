9891d277319048b8fec1e0d3a1c0aa6c1239dd45
Gaillard77 <spoorix@outlook.fr>
2025-10-10 15:56:46 +0200

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "Connexion",
@@ -13,28 +13,20 @@ export default NextAuth({
       async authorize(credentials) {
         const allowedEmail = process.env.AUTH_EMAIL;
         const allowedPassword = process.env.AUTH_PASSWORD;
-
         if (!credentials?.email || !credentials?.password) return null;
-
         if (
           credentials.email === allowedEmail &&
           credentials.password === allowedPassword
         ) {
-          // Utilisateur “virtuel” validé
           return { id: "user-1", name: "Utilisateur", email: allowedEmail };
         }
-
-        // Mauvais identifiants
         return null;
       },
     }),
-    // 👉 Tu pourras ajouter Google plus tard si tu veux :
-    // GoogleProvider({ clientId:..., clientSecret:... })
   ],
-
   session: { strategy: "jwt" },
   secret: process.env.NEXTAUTH_SECRET,
-
-  // On force l'utilisation de notre page de login custom
   pages: { signIn: "/auth/signin" },
-});
+};
+
+export default NextAuth(authOptions);
